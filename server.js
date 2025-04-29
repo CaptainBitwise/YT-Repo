9d26005936ab8c8b90c9bdb58cd95eafb6b5b2bd
Captain-JKM <alfonso.vicente@jala.university>
2025-04-28 22:19:24 -0600

    Update CI/CD deployment to Oracle VM

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://jeim-music.vercel.app/'],
+  credentials: true,
+  
+}));
 
 const API_KEY = process.env.YOUTUBE_API_KEY;
 const CHANNEL_ID = process.env.CHANNEL_ID;
